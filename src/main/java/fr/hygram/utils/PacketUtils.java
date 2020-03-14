38225b04b6a6d801ca890278ec4480f8145d31da
Felix Cravic <themode@outlook.fr>
2020-03-14 10:42:37 +0100

    Update

---
+++ src/main/java/fr/hygram/utils/PacketUtils.java
@@ -0,0 +1,18 @@
+package fr.hygram.utils;
+
+import com.github.simplenet.packet.Packet;
+import fr.hygram.packet.PacketHandler;
+import fr.hygram.packet.PacketWriter;
+
+public class PacketUtils {
+
+    public static Packet writePacket(PacketHandler packetHandler) {
+        Packet packet = Packet.builder();
+        PacketWriter packetWriter = new PacketWriter(packet);
+
+        packetHandler.write(packetWriter);
+
+        return packet;
+    }
+
+}
